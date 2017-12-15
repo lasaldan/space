b544958a7b2c7c38a46cf71e6c8ad0821f705316
Daniel Fuller <lasaldan@gmail.com>
2017-12-15 13:46:03 -0700

    Add sample compile script for production use eventually

---
+++ compile.sh
@@ -0,0 +1,14 @@
+
+files=("socket.io", "loadingScreen", "playScreen", "titleScreen", "canvas", "camera", "game")
+files_count=7
+#
+# echo '' > temp0.js
+# find ./game -name \*.js -exec cat {} >> temp0.js \;
+#
+# echo ''> temp1.js
+# echo '(function(){' > temp1.js
+# find ./bin/prop -name \*.js -exec cat {} >> temp1.js \;
+# echo '})();' >> temp1.js
+#
+# cat temp0.js temp1.js > script.js
+# rm temp1.js temp0.js
